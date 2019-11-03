65bf95d48862932f93fdfb103a97cdf7290dde41
nvelasquez <zerofull800@hotmail.com>
2019-11-03 10:56:14 -0400

    Initial commiet

---
+++ modulo2Layouts/FlexLayoutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace modulo2Layouts
+{
+    public partial class FlexLayoutPage : ContentPage
+    {
+        public FlexLayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ modulo2Layouts/ScrollViewSamplePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace modulo2Layouts
+{
+    public partial class ScrollViewSamplePage : ContentPage
+    {
+        public ScrollViewSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
